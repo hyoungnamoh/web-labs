6c920a633c817892726ac765fc24d631390e308c
hyungnam <zzzsh789@naver.com>
2020-04-28 16:43:13 +0900

    카드 짝맞추기 완성

--- javascript/js/CardMatching.js
+++ javascript/js/CardMatching.js
@@ -1,12 +1,21 @@
 const hor = 4;
 const ver = 3;
-const candidateColors = ['red', 'orange', 'green', 'yellow', 'white', 'pink', 'red', 'orange', 'green', 'yellow', 'white', 'pink'];
-let color = [];
-for(let i=0; candidateColors.length > 0; i++){
-    color = color.concat(candidateColors.splice(Math.floor(Math.random() * candidateColors.length), 1));
+const colors = ['red', 'orange', 'green', 'yellow', 'white', 'pink', 'red', 'orange', 'green', 'yellow', 'white', 'pink'];
+let candidateColors = colors.slice();
+let startTime;
+let clickFlag = false;
+let clickCard = []; //클릭 시도한 한 페어 카드
+let completeCard = [];
+let color = []; //섞은 카드 색상
+
+const shuffle = () => {
+    for(let i=0; candidateColors.length > 0; i++){
+        color = color.concat(candidateColors.splice(Math.floor(Math.random() * candidateColors.length), 1));
+    }
 }
 console.log(color);
 const cardSetting = (hor, ver) => {
+    shuffle();
     for(let i = 0; i < hor * ver; i++){
         let card = document.createElement('div');
         card.className = 'card';
@@ -16,15 +25,61 @@ const cardSetting = (hor, ver) => {
         cardFront.className = 'card-front';
         let cardBack = document.createElement('div');
         cardBack.className = 'card-back';
-
+        cardBack.style.backgroundColor = color[i];
         cardInner.appendChild(cardFront);
         cardInner.appendChild(cardBack);
         card.appendChild(cardInner);
         card.addEventListener('click', () => {
-            card.classList.toggle('flipped'); //toggle switch 같은 개념
+            //완성카드에 들어있지 않으면
+            if(clickFlag && !completeCard.includes(card)){
+                card.classList.toggle('flipped'); //toggle switch 같은 개념
+                clickCard.push(card);
+                if(clickCard.length > 1){ //두개 골랐을 때
+                    //짝이 같을 때
+                    if(clickCard[0].querySelector('.card-back').style.backgroundColor === clickCard[1].querySelector('.card-back').style.backgroundColor){
+                        completeCard.push(clickCard[0]);
+                        completeCard.push(clickCard[1]);
+                        clickCard = [];
+                        if(completeCard.length > 11){
+                            let endTime = new Date();
+                            alert((endTime - startTime)/1000 + '초 걸렸습니다.');
+                            document.querySelector('#wrapper').innerHTML = '';
+                            color = [];
+                            candidateColors = colors.slice();
+                            shuffle();
+                            clickFlag = true;
+                            clickCard = [];
+                            completeCard = [];
+                            clickFlag = false;
+                            cardSetting(hor, ver);
+                        }
+                    } else{ //짝이 틀릴 때
+                        clickFlag = false;
+                        setTimeout(() => {
+                            clickCard[0].classList.remove('flipped');
+                            clickCard[1].classList.remove('flipped');
+                            clickFlag = true;
+                            clickCard = [];
+                        }, 1000);
+                    }
+                }
+            }
         })
-        document.body.appendChild(card);
+        document.querySelector('#wrapper').appendChild(card);
     }
+    document.querySelectorAll('.card').forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.add('flipped');
+        }, 1000 + 100 * index);
+
+        setTimeout(() => {
+            card.classList.remove('flipped');
+            startTime = new Date();
+            clickFlag=true;
+        }, 5000)
+    });
 }
+
+
 
 cardSetting(hor, ver);
